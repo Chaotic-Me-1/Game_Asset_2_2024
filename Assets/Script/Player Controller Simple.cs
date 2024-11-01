f0dc8fdf5622b0b091358f19d4f6e7e971ea6822
Chaotic-Me-1 <versan00851@stud.noroff.no>
2024-11-01 14:15:54 +0100

    uppdate script can now slow down with G

--- Assets/Script/Player Controller Simple.cs
+++ Assets/Script/Player Controller Simple.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class SimplePlayerController : MonoBehaviour
 {
     // code mix John & Veronica
-    public float moveSpeed = 10;
-    public float turnSpeed = 100;
+    public float moveSpeed = 10; //default movment speed 
+    public float sneakSpeed = 5; // have a slower speed then the walk
+    public float turnSpeed = 100; // default turning speed 
+    public bool issneaking = false; //is the charature in a sneaking state
+    public float currentSpeed = 0; //stablish the state of speed takes place
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +20,31 @@ public class SimplePlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get the axis from unity to get horizontal to move the char
         float turn = Input.GetAxis("Horizontal");
         float move = Input.GetAxis("Vertical");
 
         transform.Rotate(transform.up * turn * turnSpeed * Time.deltaTime);
-        Vector3 newpos = transform.position + (transform.forward * move * moveSpeed * Time.deltaTime);
+        Vector3 newpos = transform.position + (transform.forward * move * currentSpeed * Time.deltaTime);
         transform.position = newpos;
 
-        // test sneak add animation later
-        if (Input.GetKeyDown(KeyCode.G))
-        {
+        // Sneak with pressing G
+        issneaking = Input.GetKey(KeyCode.G);
 
+        // check if sneaking
+        if (issneaking)
+        {
+            //changing the speed of sneak
+            currentSpeed = sneakSpeed;
             Debug.Log("You cliked sneak");
         }
 
+        else
+        {
+            //make the speed back to normal
+            currentSpeed = moveSpeed;
+
+        }
 
     }
 }
