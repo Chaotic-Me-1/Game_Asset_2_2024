3332f0a26de4a2fb3196e2b4195cb54b7f65cd89
Chaotic-Me-1 <versan00851@stud.noroff.no>
2024-12-02 22:57:34 +0100

    save

--- Assets/Scripts/Show UI Phone.cs
+++ Assets/Scripts/Show UI Phone.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShowUIPhone : MonoBehaviour
-{
-    public bool IsMainShowing;
-    public GameObject Mail;
+{   //Veronicas code
+    public bool IsMainShowing; // checking if the main is showing 
+    public GameObject Mail; //Getting the canvas object
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   //setting the statment at ture when the game start 
         IsMainShowing = true;  
     }
 
     // Update is called once per frame
     void Update()
     {
+        //The player can lose the UI when they are done 
+        //Making it a one time event 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             IsMainShowing = false;
