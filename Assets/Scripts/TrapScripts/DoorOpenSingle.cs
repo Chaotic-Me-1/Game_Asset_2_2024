bb3c77a279530193d76cd7f86d8cd00cb2a01449
OdinIMG2 <Odinhegdahl@hotmail.com>
2024-12-03 22:10:15 +0100

    + single door script

---
+++ Assets/Scripts/TrapScripts/DoorOpenSingle.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DoorOpenSingle : MonoBehaviour
+{
+    public Animation SDAnimation;
+    public bool open;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+             open = true;
+        }
+        if (other.tag == ("Player") &&
+            open == true)
+        {
+            SDAnimation.Play();
+        }
+     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            open = false;
+        }
+    }
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
