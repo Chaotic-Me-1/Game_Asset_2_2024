ad8202e3e08c598420fb2ab0c56119cd88d99467
OdinIMG2 <Odinhegdahl@hotmail.com>
2024-12-03 15:40:15 +0100

    Scripts

---
+++ Assets/Scripts/TrapScripts/Doorunlock_animation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorunlock_animation : MonoBehaviour
+{
+    public Animation doorAnimation;
+    //public TrapTrigger trigger;
+    public CodeUnlocked Unlocked;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Animator a = game.GetComponent<Animator>();
+
+
+        //if (trigger.trap == true) 
+        //{
+        //    doorAnimation.Play();
+
+        //}    
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {       // a simple trigger that when you have Interacted CodeUnlocked the animation will play when entering  
+        if (other.tag == ("Player") &&
+            Unlocked.isUnlocked == true)
+        {
+            doorAnimation.Play();
+
+
+
+        }
+    }
+}
