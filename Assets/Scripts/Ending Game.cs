e14a6c31962a824d27451c4122d055e29e6f8c41
Chaotic-Me-1 <versan00851@stud.noroff.no>
2024-12-03 01:00:21 +0100

    finish ending script

--- Assets/Scripts/Ending Game.cs
+++ Assets/Scripts/Ending Game.cs
@@ -7,24 +7,53 @@ public class EndingGame : MonoBehaviour
 {
     public bool IsPlayerInsideEnd = false;
     public GameObject Choice;
+    public bool IsChoiceShowing;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        IsChoiceShowing = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            IsPlayerInsideEnd = true;
+            IsChoiceShowing = true;
+            Choice.SetActive(IsChoiceShowing);
+
+        }
+      
+    }
+    private void OnTriggerExit(Collider other)
+
+    {
+        if (other.CompareTag("Player"))
+        {
+            IsPlayerInsideEnd = false;
+            IsChoiceShowing = false;
+            Choice.SetActive(IsChoiceShowing);
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-    
-    }
-    private void OnTriggerEnter (Collider other)
-    {
-        if (other.CompareTag("Player"))
+        if (IsPlayerInsideEnd)
         {
-
+            if (Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.O))
+            {
+                Debug.Log("Game end");
+                QuitGame();
+            }
         }
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
     }
 }
 
