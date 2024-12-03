04a69ae962af71dcd27a4a852ab1496d9359fb3d
OdinIMG2 <Odinhegdahl@hotmail.com>
2024-12-03 21:48:10 +0100

    +Door computer

--- Assets/Scripts/TrapScripts/Doorunlock_animation.cs
+++ Assets/Scripts/TrapScripts/Doorunlock_animation.cs
@@ -7,36 +7,55 @@ public class Doorunlock_animation : MonoBehaviour
     public Animator doorAnimation;
     //public TrapTrigger trigger;
     public CodeUnlocked Unlocked;
+    public bool isE_pressed;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        isE_pressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Animator a = game.GetComponent<Animator>();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isE_pressed = true;
 
-
-        //if (trigger.trap == true) 
-        //{
-        //    doorAnimation.Play();
-
-        //}    
-
-
+        }
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        isE_pressed = false;
     }
 
-    private void OnTriggerEnter(Collider other)
+    
+
+    private void OnTriggerStay(Collider other)
     {       // a simple trigger that when you have Interacted CodeUnlocked the animation will play when entering  
         if (other.tag == ("Player") &&
             Unlocked.isUnlocked == true)
         {
-            
-
-
-
+            doorAnimation.SetTrigger("OpeningTrigger");
         }
+
+
+        
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("Player"))
+            
+        {
+            doorAnimation.SetTrigger("ClosingTrigger");
+            isE_pressed = false;
+           
+        }
+
+
+
+    }
+
+
+
 }
